d99174e7ea2ba5dae44c62a42e87336e7840385d
siemesnn <christina_sijera@dlsu.edu.ph>
2024-03-07 10:30:07 +0800

    CONNECTED TO DB
    mongoose installed

--- controller/userController.js
+++ controller/userController.js
@@ -1,23 +1,42 @@
 const User = require('../model/user');
 const userjson = require('../test/users.json');
 
-exports.loginUser = (req, res) => {
+exports.loginUser = async (req, res) => {
     // Check if the user exists
-    const { username, password } = req.body;
+    try {
+        const { username, password } = req.body;
 
-    const users = Object.values(userjson.users); // Convert users object to an array
+        // Reuse the MongoDB client and database connection
+        const db = client.db(DB_NAME);
+        const users = db.collection('users');
+        const userLogin = await users.findOne({ username });
 
-    const user = users.find(user => user.username === username && user.password === password);
+        if (!userLogin) {
+            res.status(401).json({ message: "Invalid credentials!" });
+            return;
+        }
 
-    // Debugging information
-    console.log('User:', user);
+        const passwordMatch = await bcrypt.compare(password, userLogin.password);
 
-    // Send appropriate status
-    if (user) {
-        res.status(200).send({ username: username });
-        // 
-    } else {
-        res.status(401).send('Invalid username or password');
+        if (passwordMatch) {
+            if (!req.session) {
+                req.session = {};
+            }
+
+            if (req.session.authenticated) {
+                req.session.username = username;
+                res.status(201).json(req.session);
+            } else {
+                req.session.authenticated = true;
+                req.session.username = username;
+                res.status(201).json(req.session);
+            }
+        } else {
+            res.status(401).json({ message: "Invalid credentials!" });
+        }
+
+    } catch (e) {
+        res.status(500).json({ message: e.message });
     }
 };
 
