94309a00bc990544c0c0d23ad8b39e464e20fa8a
siemesnn <christina_sijera@dlsu.edu.ph>
2024-03-11 18:28:30 +0800

    Login user works but not encrypted

--- controller/userController.js
+++ controller/userController.js
@@ -1,12 +1,52 @@
 const User = require('../model/user');
-const bcrypt = require('bcrypt');
 const {client, DB_NAME } = require('../model/database');
 
 
+exports.registerUser = async (req, res) => {
+
+    const db = client.db(DB_NAME);
+    const users = db.collection('users');
+    try {
+        const { username, password, confirmPassword } = req.body;
+
+        if (password !== confirmPassword) {
+            res.status(400).json({ message: "Passwords do not match!" });
+            return;
+        }
+
+        // Check if the username already exists using Mongoose
+        const existingUser = await users.findOne({ username });
+
+        if (existingUser) {
+            res.status(400).json({ message: "Username is already taken!" });
+            return;
+        }
+
+        // Hash the password before storing it in the database
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        // Create a new user document using the Mongoose model
+        const newUser = new User({
+            username,
+            password: hashedPassword,
+            bio: '',
+            memberURL: 'u/' + username,
+            avatar: 'https://www.redditstatic.com/avatars/avatar_default_02_4856A3.png',
+            likedPosts: [],
+            dislikedPosts: [],
+        });
+
+        // Save the new user to the database
+        await users.insertOne(newUser);
+
+        res.json({ message: "Registration successful" });
+    } catch (e) {
+        res.status(500).json({ message: e.message });
+    }
+};
 
 
 exports.loginUser = async (req, res) => {
-    
     try {
         const { username, password } = req.body;
 
@@ -20,29 +60,28 @@ exports.loginUser = async (req, res) => {
             return;
         }
 
-        const passwordMatch = await bcrypt.compare(password, userLogin.password);
-
-        if (passwordMatch) {
-            if (!req.session) {
-                req.session = {};
-            }
-
-            if (req.session.authenticated) {
-                req.session.username = username;
-                res.status(201).json(req.session);
-            } else {
-                req.session.authenticated = true;
-                req.session.username = username;
-                res.status(201).json(req.session);
-            }
-        } else {
+        if (userLogin.password !== password) { 
             res.status(401).json({ message: "Invalid credentials!" });
+            return;
         }
+
+        // Check if req.session exists, if not, initialize it
+        if (!req.session) {
+            req.session = {};
+        }
+
+        req.session.username = username;
+        req.session.authenticated = true;
+        res.status(201).json(req.session);
 
     } catch (e) {
         res.status(500).json({ message: e.message });
     }
 };
+
+
+
+
 
     exports.returnUser = (req, res) => {
         // Check if the user exists
